80892b8f448756380450bd962cbc2221ea30825e
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-14 18:44:12 +0530

    add logic check pincode  xxx xxx this pattern using regex

--- regex.js
+++ regex.js
@@ -1,10 +1,13 @@
 var read = require("readline-sync");
 
 
-console.log("pincode is atleast 6 digit and take one alphabetand or special character at end  ")
-const re = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{2}[A-Z!@#\$%\^\&*\]{1}$");
+console.log("pincode is atleast 6 digit and pin take 'xxx xxx' this pattern true ")
+const re = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$");
 var pin=read.question("enter the pin :");
 let result = re.test(pin);
 console.log(result);
+
+
+
 
 
