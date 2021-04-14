353c1da08c0cd70ad31cd4194de243f0bac914f7
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-14 18:41:57 +0530

    add logic check pin atleast 1 character or special character at end  or 6  digit using regex

--- regex.js
+++ regex.js
@@ -1,12 +1,10 @@
 var read = require("readline-sync");
 
-console.log("pincode is atleast 6 digit and take one alphabetand or special character at biginning  ")
-const re = RegExp("^[A-Z!@#\$%\^\&*\]{1}[0-9]{2}[\\s]?[0-9]{3}$");
+
+console.log("pincode is atleast 6 digit and take one alphabetand or special character at end  ")
+const re = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{2}[A-Z!@#\$%\^\&*\]{1}$");
 var pin=read.question("enter the pin :");
 let result = re.test(pin);
 console.log(result);
-
-
-
 
 
